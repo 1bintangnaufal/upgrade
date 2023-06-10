60b2a0fc75eaab39811a675047cf02089ea45178
Bintang <1bintangnaufal@gmail.com>
2023-06-10 23:10:56 +0700

    Struct & Interface Manipulation

--- Public/Assets/JS/Project-Form.js
+++ Public/Assets/JS/Project-Form.js
@@ -1,135 +1,77 @@
-let project_data = [];
+async function Post_Project(event) {
+  event.preventDefault();
+  let Project_Title = document.getElementById("Project_Title").value;
+  if (Project_Title.length > 30) {
+    alert("Maximum Characters For Project Title Exceeds 30.");
+    return;
+  }
+  let Start_Date = document.getElementById("Start_Date").value;
+  let Finish_Date = document.getElementById("Finish_Date").value;
+  let Description = document.getElementById("Description").value;
+  if (Description.length < 80) {
+    alert("Minimum Characters For Project Description (80) Not Fullfilled.");
+    return;
+  }
+  let Today = new Date().toLocaleDateString().split("/").join("-");
+  let Current_Date = new Date();
+  let Selected_Finish_Date = new Date(Finish_Date);
+  if (Selected_Finish_Date > Current_Date) {
+    alert("Time travel is not yet invented.");
+    return;
+  }
+  let Switch_Toggle = document.querySelectorAll(".form-check-input:checked");
+  if (Switch_Toggle.length === 0) {
+    alert("Select at least one technology used.");
+    return;
+  }
+  let SD_Validation = new Date(Start_Date);
+  let FD_Validation = new Date(Finish_Date);
+  if (
+    isNaN(SD_Validation) ||
+    isNaN(FD_Validation) ||
+    SD_Validation > FD_Validation
+  ) {
+    alert("Please input your dates correctly.");
+    return;
+  }
+  let Form_Data = new FormData(event.target);
+  try {
+    const Response = await fetch("/", {
+      method: "POST",
+      body: Form_Data,
+    });
+    if (Response.ok) {
+      localStorage.setItem("Tasty_Toasty_Toast", "true");
+      Close_Modal();
+      location.reload();
+    } else {
+      alert("Fetch failed.");
+    }
+  } catch (error) {
+    alert("Oh no....");
+    console.error(error);
+  }
+}
 
-function post_project(event) {
-    event.preventDefault();
+function Close_Modal() {
+  const Project_Form_Modal = document.getElementById("add-new");
+  const Bootstrap_Modal = bootstrap.Modal.getInstance(Project_Form_Modal);
+  Bootstrap_Modal.hide();
+}
 
-    let project_title = document.getElementById("project-title").value;
-    if (project_title.length > 30) {
-        return alert("Maximum Characters For Project Title Exceeds 30.")
-    };
-
-    let start_date = document.getElementById("start-date").value;
-    let finish_date = document.getElementById("finish-date").value;
-    let description = document.getElementById("description").value;
-    if(description.length < 80) {
-        return alert("Minimum Characters For Project Description (80) Not Fullfilled")
-    };
-
-    let upload_image = document.getElementById("upload-image").files;
-
-    let today = new Date().toLocaleDateString().split("/").join("-");
-    if (finish_date > today) {
-        return alert("Time travel is not yet invented.");
-    };
-
-    const js_i = '<i class="fa-brands fa-square-js fa-lg fa-fw"></i>';
-    const bs_i = '<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>';
-    const go_i = '<i class="fa-brands fa-golang fa-lg fa-fw"></i>';
-    const react_i = '<i class="fa-brands fa-react fa-lg fa-fw"></i>';
-
-    let form_check_input = document.querySelectorAll(".form-check-input:checked");
-    if (form_check_input.length === 0) {
-        return alert("Select at least one technology used.");
-    };
-
-    let js_check = document.getElementById("js-check").checked ? js_i : "";
-    let bs_check = document.getElementById("bs-check").checked ? bs_i : "";
-    let go_check = document.getElementById("go-check").checked ? go_i : "";
-    let react_check = document.getElementById("react-check").checked ? react_i : "";
-
-    upload_image = URL.createObjectURL(upload_image[0]);
-
-    const sd_validation = new Date(start_date);
-    const fd_validation = new Date(finish_date);
-    if (sd_validation > fd_validation) {
-        return alert("Please input your dates correctly.");
-    };
-
-    let ppc = {
-        project_title,
-        start_date,
-        finish_date,
-        description,
-        js_check,
-        bs_check,
-        go_check,
-        react_check,
-        upload_image,
-    };
-
-    const get_toast = document.getElementById("toast-container");
-    const show_toast = new bootstrap.Toast(get_toast);
-    show_toast.show();
-
-    project_data.push(ppc);
-    console.log(project_data);
-
-    render_ppc();
-
-    close_modal();
+window.onload = () => {
+  if (localStorage.getItem("Tasty_Toasty_Toast") === "true") {
+    const Get_Toast = document.getElementById("toast-container");
+    const Show_Toast = new bootstrap.Toast(Get_Toast);
+    Show_Toast.show();
+    localStorage.removeItem("Tasty_Toasty_Toast");
+    Auto_Scroll();
+  }
 };
 
-function render_ppc() {
-    document.getElementById("ppc-container").innerHTML = "";
-
-    for (let index = 0; index < project_data.length; index++) {
-        const start_date = new Date(project_data[index].start_date);
-        const finish_date = new Date(project_data[index].finish_date);
-        const remainder = finish_date - start_date;
-        const time_units = [
-            { value: 365.25 * 24 * 60 * 60 * 1000, label: "year(s)" },
-            { value: 30 * 24 * 60 * 60 * 1000, label: "month(s)" },
-            { value: 7 * 24 * 60 * 60 * 1000, label: "week(s)" },
-            { value: 24 * 60 * 60 * 1000, label: "day(s)" },
-        ];
-
-        let result = "";
-        for (let calculation = 0; calculation < time_units.length; calculation++) {
-            const { value, label } = time_units[calculation];
-            const calculate = Math.floor(remainder / value);
-            if (calculate > 0) {
-                result = `${calculate} ${label}`;
-                break;
-            };
-        };
-
-        if (result === "") {
-            result = "Less than a day";
-        };
-
-        document.getElementById("ppc-container").innerHTML += `
-        <div class="rounded-4 border-0 shadow-sm ppc" style="width: 16em;">
-            <img src="${project_data[index].upload_image}" class="card-img-top rounded-top-4" alt="Mobile App" style="height: 10.25em; object-fit: cover;">
-            <div class="card-body p-3">
-              <a href="/Project-Detail/:id" class="card-title ppc-title" style="text-decoration: none;">
-                <h6 class="text-truncate">${project_data[index].project_title}</h6>
-              </a>
-              <p class="text-muted" style="font-size: small; line-height: .5;">${result}</p>
-              <p class="card-text lh-sm"
-                style="font-size: small; display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden;">
-                ${project_data[index].description}</p>
-              <div class="d-flex gap-2 my-4">
-                ${project_data[index].js_check}
-                ${project_data[index].bs_check}
-                ${project_data[index].go_check}
-                ${project_data[index].react_check}
-              </div>
-              <div class="d-flex flex-row gap-3">
-                <button class="btn rounded-pill btn-outline-secondary btn-sm w-50">Edit</button>
-                <button class="btn rounded-pill btn-outline-danger btn-sm w-50">Delete</button>
-              </div>
-            </div>
-          </div>
-        `;
-    };
-
-    document.getElementById("ppc-container").scrollIntoView({ behavior: 'smooth' });
-};
-
-function close_modal() {
-    const project_form_modal = document.getElementById("add-new");
-
-    const bootstrap_modal = bootstrap.Modal.getInstance(project_form_modal);
-    bootstrap_modal.hide();
-};
-
+function Auto_Scroll() {
+  const PPC_Container = document.getElementById("ppc-container");
+  const PPC = PPC_Container.getElementsByClassName("ppc");
+  const Recent_Card = PPC[PPC.length - 1];
+  Recent_Card.scrollIntoView({ behavior: "smooth" });
+}
--- main.go
+++ main.go
@@ -1,60 +1,376 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"text/template"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Project struct {
+	Project_Title string
+	Start_Date    string
+	Finish_Date   string
+	Duration      string
+	Description   string
+	Toggle_A      bool
+	Toggle_B      bool
+	Toggle_C      bool
+	Toggle_D      bool
+	Icon_A        string
+	Icon_B        string
+	Icon_C        string
+	Icon_D        string
+	Label_A       string
+	Label_B       string
+	Label_C       string
+	Label_D       string
+}
+
+var Static_Project_Data = []Project{
+	{
+		Project_Title: "Mobile App - 2023",
+		Start_Date:    "2023-01-17",
+		Finish_Date:   "2023-05-17",
+		Duration:      "4 months",
+		Description:   "Quasi, iusto autem voluptas, facilis quidem aliquid sed harum provident iure nobis eaque sin accusantium excepturi consequatur, amet totam magni blanditiis. Voluptatibus natus placeat, maiores voluptates distinctio quia. Saepe porro maxime iste maiores voluptatem deserunt alias, debitis dolore odit eaque. Cupiditate architecto eaque vero debitis velit unde, cum haru consequatur iste. Neque molestias ducimus temporibus ex labore tempore magni, provident maiores? Obcaecati, placeat aliquid, officiis nulla voluptatem asperiores, ipsa unde a vel magni facere inventore necessitatibus.",
+		Icon_A:        `<i class="fa-brands fa-square-js fa-lg fa-fw"></i>`,
+		Icon_B:        `<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>`,
+		Icon_C:        `<i class="fa-brands fa-golang fa-lg fa-fw"></i>`,
+		Icon_D:        `<i class="fa-brands fa-react fa-lg fa-fw"></i>`,
+		Label_A:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-square-js fa-lg fa-fw"></i>
+							<p class="m-0">Javascript</p>
+						</div>
+						`,
+		Label_B:       `<div class="d-flex flex-row align-items-center gap-1">
+            				<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>
+            				<p class="m-0">Bootstrap</p>
+        				</div>
+						`,
+		Label_C:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-golang fa-lg fa-fw"></i>
+							<p class="m-0">Go</p>
+						</div>
+						`,
+		Label_D:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-react fa-lg fa-fw"></i>
+							<p class="m-0">React</p>
+						</div>
+						`,
+	},
+	{
+		Project_Title: "Web App - 2022",
+		Start_Date:    "2022-04-11",
+		Finish_Date:   "2022-06-22",
+		Duration:      "2 months",
+		Description:   "Quasi, iusto autem voluptas, facilis quidem aliquid sed harum provident iure nobis eaque sin accusantium excepturi consequatur, amet totam magni blanditiis. Voluptatibus natus placeat, maiores voluptates distinctio quia. Saepe porro maxime iste maiores voluptatem deserunt alias, debitis dolore odit eaque. Cupiditate architecto eaque vero debitis velit unde, cum haru consequatur iste. Neque molestias ducimus temporibus ex labore tempore magni, provident maiores? Obcaecati, placeat aliquid, officiis nulla voluptatem asperiores, ipsa unde a vel magni facere inventore necessitatibus.",
+		Icon_A:        `<i class="fa-brands fa-square-js fa-lg fa-fw"></i>`,
+		Icon_B:        `<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>`,
+		Icon_C:        `<i class="fa-brands fa-golang fa-lg fa-fw"></i>`,
+		Icon_D:        `<i class="fa-brands fa-react fa-lg fa-fw"></i>`,
+		Label_A:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-square-js fa-lg fa-fw"></i>
+							<p class="m-0">Javascript</p>
+						</div>
+						`,
+		Label_B:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>
+							<p class="m-0">Bootstrap</p>
+						</div>
+						`,
+		Label_C:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-golang fa-lg fa-fw"></i>
+							<p class="m-0">Go</p>
+						</div>
+						`,
+		Label_D:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-react fa-lg fa-fw"></i>
+							<p class="m-0">React</p>
+						</div>
+						`,
+	},
+	{
+		Project_Title: "Desktop App - 2022",
+		Start_Date:    "2019-08-06",
+		Finish_Date:   "2022-09-08",
+		Duration:      "3 years",
+		Description:   "Quasi, iusto autem voluptas, facilis quidem aliquid sed harum provident iure nobis eaque sin accusantium excepturi consequatur, amet totam magni blanditiis. Voluptatibus natus placeat, maiores voluptates distinctio quia. Saepe porro maxime iste maiores voluptatem deserunt alias, debitis dolore odit eaque. Cupiditate architecto eaque vero debitis velit unde, cum haru consequatur iste. Neque molestias ducimus temporibus ex labore tempore magni, provident maiores? Obcaecati, placeat aliquid, officiis nulla voluptatem asperiores, ipsa unde a vel magni facere inventore necessitatibus.",
+		Icon_A:        `<i class="fa-brands fa-square-js fa-lg fa-fw"></i>`,
+		Icon_B:        `<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>`,
+		Icon_C:        `<i class="fa-brands fa-golang fa-lg fa-fw"></i>`,
+		Icon_D:        `<i class="fa-brands fa-react fa-lg fa-fw"></i>`,
+		Label_A:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-square-js fa-lg fa-fw"></i>
+							<p class="m-0">Javascript</p>
+						</div>
+						`,
+		Label_B:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>
+							<p class="m-0">Bootstrap</p>
+						</div>
+						`,
+		Label_C:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-golang fa-lg fa-fw"></i>
+							<p class="m-0">Go</p>
+						</div>
+						`,
+		Label_D:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-react fa-lg fa-fw"></i>
+							<p class="m-0">React</p>
+						</div>
+						`,
+	},
+	{
+		Project_Title: "Server Building - 2022",
+		Start_Date:    "2021-03-07",
+		Finish_Date:   "2022-05-24",
+		Duration:      "1 year",
+		Description:   "Quasi, iusto autem voluptas, facilis quidem aliquid sed harum provident iure nobis eaque sin accusantium excepturi consequatur, amet totam magni blanditiis. Voluptatibus natus placeat, maiores voluptates distinctio quia. Saepe porro maxime iste maiores voluptatem deserunt alias, debitis dolore odit eaque. Cupiditate architecto eaque vero debitis velit unde, cum haru consequatur iste. Neque molestias ducimus temporibus ex labore tempore magni, provident maiores? Obcaecati, placeat aliquid, officiis nulla voluptatem asperiores, ipsa unde a vel magni facere inventore necessitatibus.",
+		Icon_A:        `<i class="fa-brands fa-square-js fa-lg fa-fw"></i>`,
+		Icon_B:        `<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>`,
+		Icon_C:        `<i class="fa-brands fa-golang fa-lg fa-fw"></i>`,
+		Icon_D:        `<i class="fa-brands fa-react fa-lg fa-fw"></i>`,
+		Label_A:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-square-js fa-lg fa-fw"></i>
+							<p class="m-0">Javascript</p>
+						</div>
+						`,
+		Label_B:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>
+							<p class="m-0">Bootstrap</p>
+						</div>
+						`,
+		Label_C:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-golang fa-lg fa-fw"></i>
+							<p class="m-0">Go</p>
+						</div>
+						`,
+		Label_D:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-react fa-lg fa-fw"></i>
+							<p class="m-0">React</p>
+						</div>
+						`,
+	},
+	{
+		Project_Title: "Pecel Lele",
+		Start_Date:    "2023-06-07",
+		Finish_Date:   "2023-06-07",
+		Duration:      "Less than a day",
+		Description:   "Quasi, iusto autem voluptas, facilis quidem aliquid sed harum provident iure nobis eaque sin accusantium excepturi consequatur, amet totam magni blanditiis. Voluptatibus natus placeat, maiores voluptates distinctio quia. Saepe porro maxime iste maiores voluptatem deserunt alias, debitis dolore odit eaque. Cupiditate architecto eaque vero debitis velit unde, cum haru consequatur iste. Neque molestias ducimus temporibus ex labore tempore magni, provident maiores? Obcaecati, placeat aliquid, officiis nulla voluptatem asperiores, ipsa unde a vel magni facere inventore necessitatibus.",
+		Icon_A:        `<i class="fa-brands fa-square-js fa-lg fa-fw"></i>`,
+		Icon_B:        `<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>`,
+		Icon_C:        `<i class="fa-brands fa-golang fa-lg fa-fw"></i>`,
+		Icon_D:        `<i class="fa-brands fa-react fa-lg fa-fw"></i>`,
+		Label_A:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-square-js fa-lg fa-fw"></i>
+							<p class="m-0">Javascript</p>
+						</div>
+						`,
+		Label_B:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>
+							<p class="m-0">Bootstrap</p>
+						</div>
+						`,
+		Label_C:       `<div class="d-flex flex-row align-items-center gap-1">
+            				<i class="fa-brands fa-golang fa-lg fa-fw"></i>
+            				<p class="m-0">Go</p>
+        				</div>
+						`,
+		Label_D:       `<div class="d-flex flex-row align-items-center gap-1">
+							<i class="fa-brands fa-react fa-lg fa-fw"></i>
+							<p class="m-0">React</p>
+						</div>
+						`,
+	},
+}
+
 func main() {
 	e := echo.New()
 	e.Static("/Public", "Public")
-	e.Static("Source", "Source")
 	e.GET("/", Main_Page)
 	e.GET("/Project-Detail/:id", Static_Project_Detail)
 	e.POST("/", Project_Form_Value)
+	e.POST("/Delete-Project/:id", Delete_Project)
 	e.Logger.Fatal(e.Start("localhost:5000"))
 }
 
 func Main_Page(c echo.Context) error {
-	var tmpl, err = template.ParseFiles("Source/Views/Index.html")
+	var tmpl, err = template.ParseFiles("Views/Index.html")
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
-	return tmpl.Execute(c.Response(), nil)
+	Projects := map[string]interface{}{
+		"Projects": Static_Project_Data,
+	}
+	return tmpl.Execute(c.Response(), Projects)
 }
 
 func Static_Project_Detail(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	data := map[string]interface{}{
-		"ID": id,
+	var Project_Detail = Project{}
+	for i, data := range Static_Project_Data {
+		if id == i {
+			Project_Detail = Project{
+				Project_Title: data.Project_Title,
+				Start_Date:    data.Start_Date,
+				Finish_Date:   data.Finish_Date,
+				Duration:      data.Duration,
+				Description:   data.Description,
+				Label_A:       data.Label_A,
+				Label_B:       data.Label_B,
+				Label_C:       data.Label_C,
+				Label_D:       data.Label_D,
+			}
+		}
 	}
-	tmpl, err := template.ParseFiles("Source/Views/Project-Detail.html")
+	data := map[string]interface{}{
+		"Project":        Project_Detail,
+		"Date_Formation": Date_Formation,
+	}
+	tmpl, err := template.New("Project-Detail.html").Funcs(template.FuncMap{"Date_Formation": Date_Formation}).ParseFiles("Views/Project-Detail.html")
 	if err != nil {
+		fmt.Println("Template parsing error:", err)
 		return c.JSON(http.StatusInternalServerError, map[string]string{"mesage": err.Error()})
 	}
-	return tmpl.Execute(c.Response(), data)
+	err = tmpl.Execute(c.Response(), data)
+	if err != nil {
+		fmt.Println("Template execution error:", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "Internal Server Error"})
+	}
+	return nil
+}
+
+func Date_Formation(date string) string {
+	t, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return "Invalid date format"
+	}
+	return t.Format("Jan 2, 2006")
+}
+
+func Duration_Formatting(Duration time.Duration) string {
+	if Duration <= 24*time.Hour {
+		return "Less than a day"
+	}
+	Days := int(Duration.Hours() / 24)
+	Weeks := Days / 7
+	Months := Days / 30
+	Years := Months / 12
+	if Years > 1 {
+		return fmt.Sprintf("%d years", Years)
+	} else if Years == 1 {
+		return "A year"
+	} else if Months > 1 {
+		return fmt.Sprintf("%d months", Months)
+	} else if Months == 1 {
+		return "A month"
+	} else if Weeks > 1 {
+		return fmt.Sprintf("%d weeks", Weeks)
+	} else if Weeks == 1 {
+		return "A week"
+	} else if Days > 1 {
+		return fmt.Sprintf("%d days", Days)
+	} else {
+		return "A day"
+	}
+}
+
+func Fa_I(Input_Value string) string {
+	if Input_Value == "Toggle_A" {
+		return `<i class="fa-brands fa-square-js fa-lg fa-fw"></i>`
+	} else if Input_Value == "Toggle_B" {
+		return `<i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>`
+	} else if Input_Value == "Toggle_C" {
+		return `<i class="fa-brands fa-golang fa-lg fa-fw"></i>`
+	} else if Input_Value == "Toggle_D" {
+		return `<i class="fa-brands fa-react fa-lg fa-fw"></i>`
+	} else {
+		return ""
+	}
+
+}
+
+func Fa_I_Labels(Input_Value string) string {
+	if Input_Value == "Toggle_A" {
+		return `
+		<div class="d-flex flex-row align-items-center gap-1">
+			<i class="fa-brands fa-square-js fa-lg fa-fw"></i>
+			<p class="m-0">Javascript</p>
+		</div>
+		`
+	} else if Input_Value == "Toggle_B" {
+		return `
+		<div class="d-flex flex-row align-items-center gap-1">
+            <i class="fa-brands fa-bootstrap fa-lg fa-fw"></i>
+            <p class="m-0">Bootstrap</p>
+        </div>
+		`
+	} else if Input_Value == "Toggle_C" {
+		return `
+		<div class="d-flex flex-row align-items-center gap-1">
+            <i class="fa-brands fa-golang fa-lg fa-fw"></i>
+            <p class="m-0">Go</p>
+        </div>
+		`
+	} else if Input_Value == "Toggle_D" {
+		return `
+		<div class="d-flex flex-row align-items-center gap-1">
+			<i class="fa-brands fa-react fa-lg fa-fw"></i>
+			<p class="m-0">React</p>
+		</div>
+		`
+	} else {
+		return ""
+	}
 }
 
 func Project_Form_Value(c echo.Context) error {
-	Project_Title := c.FormValue("project-title")
-	Start_Date := c.FormValue("start-date")
-	Finsih_Date := c.FormValue("finish-date")
-	Description := c.FormValue("description")
-	Toggle_A := c.FormValue("input-value-js")
-	Toggle_B := c.FormValue("input-value-bs")
-	Toggle_C := c.FormValue("input-value-go")
-	Toggle_D := c.FormValue("input-value-react")
-	println(
-		Project_Title, 
-		Start_Date, 
-		Finsih_Date, 
-		Description, 
-		Toggle_A, 
-		Toggle_B, 
-		Toggle_C, 
-		Toggle_D)
+	Project_Title := c.FormValue("Project_Title")
+	Start_Date := c.FormValue("Start_Date")
+	Finish_Date := c.FormValue("Finish_Date")
+	Description := c.FormValue("Description")
+	Toggle_A := c.FormValue("Toggle_A")
+	Toggle_B := c.FormValue("Toggle_B")
+	Toggle_C := c.FormValue("Toggle_C")
+	Toggle_D := c.FormValue("Toggle_D")
+	Icon_A := Fa_I(Toggle_A)
+	Icon_B := Fa_I(Toggle_B)
+	Icon_C := Fa_I(Toggle_C)
+	Icon_D := Fa_I(Toggle_D)
+	Label_A := Fa_I_Labels(Toggle_A)
+	Label_B := Fa_I_Labels(Toggle_B)
+	Label_C := Fa_I_Labels(Toggle_C)
+	Label_D := Fa_I_Labels(Toggle_D)
+	Start_Date_Format, _ := time.Parse("2006-01-02", Start_Date)
+	Finish_Date_Format, _ := time.Parse("2006-01-02", Finish_Date)
+	Duration := Finish_Date_Format.Sub(Start_Date_Format)
+	Duration_Format := Duration_Formatting(Duration)
+	var Render_New_Project = Project{
+		Project_Title: Project_Title,
+		Start_Date:    Start_Date,
+		Finish_Date:   Finish_Date,
+		Description:   Description,
+		Icon_A:        Icon_A,
+		Icon_B:        Icon_B,
+		Icon_C:        Icon_C,
+		Icon_D:        Icon_D,
+		Label_A:       Label_A,
+		Label_B:       Label_B,
+		Label_C:       Label_C,
+		Label_D:       Label_D,
+		Duration:      Duration_Format,
+	}
+	fmt.Println(Render_New_Project)
+	Static_Project_Data = append(Static_Project_Data, Render_New_Project)
+	return c.Redirect(http.StatusMovedPermanently, "/")
+}
+
+func Delete_Project(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	Static_Project_Data = append(Static_Project_Data[:id], Static_Project_Data[id+1:]... )
 	return c.Redirect(http.StatusMovedPermanently, "/")
 }
